8af28f9967af30831db278816e4c1dcee6620317
AndreyTishchenko <Tishchenko.Andrew@yandex.ru>
2024-10-13 15:04:06 +0400

    feat: add rm command

--- commandHandler.js
+++ commandHandler.js
@@ -4,6 +4,7 @@ import up from './commands/up.js';
 import ls from './commands/ls.js';
 import cat from './commands/cat.js';
 import add from './commands/add.js';
+import rm from './commands/rm.js';
 import osFunc from './commands/os.js';
 import hash_calculator from './commands/hash_calculator.js';
 async function commandHandler(command, payload) {
@@ -37,6 +38,14 @@ async function commandHandler(command, payload) {
             }
             await add(payload)
             break
+        case 'rm':
+            if (payload.length == 0) {
+                throw new Error('Invalid input');
+            }
+            rm(payload).catch((error) => {
+                console.log(error.message);
+            })
+            break;
         case 'os':
             await osFunc(payload);
             break;
