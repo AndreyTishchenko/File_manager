323d30d921679805c0cc6a38e252ecd27612db43
AndreyTishchenko <Tishchenko.Andrew@yandex.ru>
2024-10-14 01:21:26 +0400

    feat: create function mv

--- commandHandler.js
+++ commandHandler.js
@@ -5,6 +5,7 @@ import ls from './commands/ls.js';
 import cat from './commands/cat.js';
 import add from './commands/add.js';
 import rm from './commands/rm.js';
+import mv from './commands/mv.js';
 import cp from './commands/cp.js';
 import osFunc from './commands/os.js';
 import hash_calculator from './commands/hash_calculator.js';
@@ -50,6 +51,9 @@ async function commandHandler(command, payload) {
             rm(payload).catch((error) => {
                 console.log(error.message);
             })
+            break;
+        case 'mv':
+            await mv(payload);
             break;
         case 'os':
             await osFunc(payload);
